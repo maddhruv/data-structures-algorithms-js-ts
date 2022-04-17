e6679ce1ce5cb15f0c0f59f7ddfe192aa7656e5c
Dhruv Jain <dhruvjainpenny@gmail.com>
2022-04-17 08:58:02 +0530

    fix: tree

--- tree/index.ts
+++ tree/index.ts
@@ -69,4 +69,40 @@ class BST<T> {
       }
     }
   }
+
+  public findMin() {
+    let current = this.root;
+
+    while (current.left !== null) {
+      current = current.left;
+    }
+
+    return current.data;
+  }
+
+  public findMax() {
+    let current = this.root;
+
+    while (current.right !== null) {
+      current = current.right;
+    }
+
+    return current.data;
+  }
+
+  public isPresent(data: T) {
+    let current = this.root;
+
+    while (current) {
+      if (data === current.data) {
+        return true;
+      } else if (data < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+
+    return false;
+  }
 }
